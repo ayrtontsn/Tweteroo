113f7032a03e8f4361daa8a04963a93fe9eae232
Ayrton Tales Silva do Nascimento <ayrtontsn@hotmail.com>
2024-09-20 16:08:02 -0300

    Feat: Implementar put e alteração de eficiencia no codigo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import httpStatus from "http-status"
 import { MongoClient, ObjectId } from "mongodb"
 import dotenv from "dotenv"
-import Joi from "joi"
+import joi from "joi"
 
 dotenv.config();
 const databaseURL = process.env.DATABASE_URL;
@@ -22,12 +22,31 @@ mongoClient.connect()
 
 app.post("/sign-up", async (req, res) => {
     const user = req.body;
+
+    const signUpSchema = joi.object({
+        username: joi.string().min(1).max(16).required(),
+        avatar: joi.string().required()
+    })
+
     try {
+        const validation = signUpSchema.validate(user, { abortEarly: false })
+
+        if (validation.error) {
+            const errors = validation.error.details.map((detail) => detail.message);
+
+            console.log(errors)
+            return res.sendStatus(httpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        const newuser = await db.collection("users").findOne({ username: user.username });
+        if (newuser) {
+            return res.sendStatus(httpStatus.NO_CONTENT);
+        };
+
         await db.collection("users").insertOne({
             username: user.username,
             avatar: user.avatar
         });
-        console.log("Deu certo o login")
         return res.sendStatus(httpStatus.CREATED);
 
     } catch (error) {
@@ -35,37 +54,41 @@ app.post("/sign-up", async (req, res) => {
     }
 })
 
-app.post("/tweets", async(req,res)=>{
+app.post("/tweets", async (req, res) => {
     const msg = req.body;
+    try {
+        const usertweet = await db.collection("users").findOne({ username: msg.username });
+        if (!usertweet) {
+            return res.sendStatus(httpStatus.UNAUTHORIZED);
+        };
+        const avatar = usertweet.avatar;
 
-    const usertweet = await db.collection("users").findOne({username: msg.username});
-    if (!usertweet){
-        return res.status(httpStatus.UNAUTHORIZED);
-    }
-    const avatar = usertweet.avatar;
+        const userSchema = joi.object({
+            username: joi.string().required(),
+            avatar: joi.string().required(),
+            tweet: joi.string().min(1).required()
+        })
 
-    const schema =Joi.object({
-        username: Joi.string().required(),
-        avatar: Joi.string().required(),
-        tweet: Joi.string().required()
-    })
-
-    try {       
-        const tweet = await db.collection("tweets").insertOne({
+        const tweet = {
             username: msg.username,
             avatar,
             tweet: msg.tweet
-        })
+        }
+        const validation = userSchema.validate(tweet, { abortEarly: false })
 
-        schema.validate(tweet, {abortEarly: false})
+        if (validation.error) {
+            const errors = validation.error.details.map((detail) => detail.message);
+            return res.status(422).send(errors);
+        }
+        await db.collection("tweets").insertOne(tweet)
 
         return res.sendStatus(httpStatus.CREATED)
     } catch (error) {
-        return res.status(httpStatus.UNPROCESSABLE_ENTITY);
+        return res.send(error.message);
     }
 })
 
-app.get("/tweets", async(req,res) => {
+app.get("/tweets", async (req, res) => {
     await db.collection("tweets").find().toArray()
         .then(tweets => {
             return res.send(tweets.reverse())
@@ -73,18 +96,49 @@ app.get("/tweets", async(req,res) => {
         .catch(err => res.send(err))
 })
 
-app.delete("/tweets/:id", async(req,res) =>{
+app.delete("/tweets/:id", async (req, res) => {
     const { id } = req.params;
 
     try {
-        const tweetdel = await db.collection("tweets").deleteOne({ _id: new ObjectId(id)})
-
-        if (tweetdel.deletedCount === 0) {
-            console.log("não deletou")}
+        const tweet = await db.collection("tweets").deleteOne({ _id: new ObjectId(id) })
+        if(!tweet){
+            return res.sendStatus(httpStatus.NOT_FOUND);
+        }
 
         return res.sendStatus(httpStatus.NO_CONTENT)
     } catch (error) {
         res.sendStatus(httpStatus.BAD_REQUEST)
+    }
+})
+
+app.put("/tweets/:id", async (req, res) => {
+    const msg = req.body;
+    const { id } = req.params;
+    let newmsg = msg.tweet
+
+    try {
+        const tweet = await db.collection("tweets").findOne({ _id: new ObjectId(id) })
+        if(!tweet){
+            return res.sendStatus(httpStatus.NOT_FOUND);
+        }
+
+        const newtweet = {
+            username: tweet.username,
+            avatar: tweet.avatar,
+            tweet: newmsg
+        }
+
+        if (!newmsg) newtweet.tweet = tweet.tweet
+
+        await db.collection("tweets").updateOne(
+            { _id: new ObjectId(id) }, {
+            $set: newtweet
+        })
+
+        return res.sendStatus(httpStatus.NO_CONTENT)
+
+    } catch (error) {
+        res.sendStatus(httpStatus.NOT_FOUND)
     }
 })
 
